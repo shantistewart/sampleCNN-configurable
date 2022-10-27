d82e003420350d9d6ffe81298be1b06fe1e37753
shantistewart <shantiystewart@gmail.com>
2022-10-27 12:58:35 -0700

    Added saving model summary to file.

--- scripts/model_summary.py
+++ scripts/model_summary.py
@@ -1,9 +1,14 @@
 """Script to display the summary of the SampleCNN model."""
 
 
+import os
 from torchinfo import summary
 from model.sample_cnn import SampleCNN
 
+
+# model summaries directory and file name (summary file path = /summaries_dir/summary_file_name):
+summaries_dir = "model_summaries"
+summary_file_name = "sample_cnn_summary.txt"
 
 # model architecture hyperparameters:
 n_blocks = 9
@@ -16,18 +21,19 @@ first_block_params = {
     "out_channels": 128,
     "conv_size": 3
 }
-# input dimensions:
 input_size = 59049
+
+# model summary options:
 batch_size = 1
-# depth of nested layers to display:
 summary_depth = 3
+summary_info = ["input_size", "output_size", "num_params"]
 
 
 if __name__ == "__main__":
     print("\n")
 
     # create SampleCNN:
-    sample_cnn = SampleCNN(
+    model = SampleCNN(
         n_blocks=n_blocks,
         n_channels=n_channels,
         output_size=output_size,
@@ -38,7 +44,22 @@ if __name__ == "__main__":
         input_size=input_size,
     )
 
-    # print model summary:
-    summary(sample_cnn, input_size=(batch_size, 1, input_size), depth=summary_depth)
+    # create model summary:
+    model_summary = str(summary(
+        model,
+        input_size=(batch_size, 1, input_size),
+        col_names=summary_info,
+        depth=summary_depth,
+        verbose=0
+    ))
+
+    # save model summary to output file:
+    os.makedirs(summaries_dir, exist_ok=True)
+    summary_file = os.path.join(summaries_dir, summary_file_name)
+    with open(summary_file, "w") as text_file:
+        text_file.write(model_summary)
+    # display model summary:
+    print("MODEL SUMMARY:\n")
+    print(model_summary)
     print("\n")
 
